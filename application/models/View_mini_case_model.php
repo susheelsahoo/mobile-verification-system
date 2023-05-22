36af4dd99925b82c431766bdef7c647e888b3648
Yogi1606 <yogitasharma1606@gmail.com>
2023-05-22 07:16:25 +0000

    gfdgd

--- application/models/View_mini_case_model.php
+++ application/models/View_mini_case_model.php
@@ -142,5 +142,28 @@ class View_mini_case_model extends CI_Model
 			throw $ex;
 		}
 	}
+	
+	function update_bv_remarks($bv_id, $data)
+	{
+		try {
+			$this->db->where("id", $bv_id);
+			// print_r($data);die;
+			$return_data = $this->db->update("mini_case", $data);
+			return $return_data;
+		} catch (Exception $ex) {
+			throw $ex;
+		}
+	}
+
+    function fetch_bv_remarks($user_id)
+	{
+		try {
+			$this->db->where("id", $user_id);
+			$query = $this->db->get('mini_case');
+			return $query->result();
+		} catch (Exception $ex) {
+			throw $ex;
+		}
+	}
 
 }
--- application/views/view_mini_case.php
+++ application/views/view_mini_case.php
@@ -57,8 +57,8 @@
                     "orderable": false
                 }],
                 "lengthMenu": [
-                    [10, 25, 50, -1],
-                    [10, 25, 50, "All"]
+                    [15, 25, 50, -1],
+                    [15, 25, 50, "All"]
                 ],
                 // createdRow: function(row, data, rowIndex) {
                 //     $.each($('td', row), function(colIndex) {
@@ -267,6 +267,7 @@
             });
 
             // end update form data 
+            
             $(document).on('click', '.edit_rv', function() {
                 var user_id = $(this).attr("id");
                 $.ajax({
@@ -281,6 +282,107 @@
                         $('#u_rv_remarks').val(data.rv_remarks);
                         $('#u_rv_id').val(user_id);
                         $('#update_rv_remarks').val("edit");
+                    }
+                });
+            });
+
+    // update the form data if we change any
+            $('#update_bv_remarks_form').submit(function(e) {
+                // alert("click on update button");
+                e.preventDefault();
+                var me = $(this);
+                var user_id = $(this).attr("id");
+
+                // perform ajax
+                $.ajax({
+                    url: me.attr('action'),
+                    type: 'POST',
+                    data: me.serialize(),
+                    // data:new FormData(this),  
+                    dataType: 'json',
+                    success: function(response) {
+                        if (response.success == true) {
+                            alert("Remarks Updated Successfully!");
+                            location.reload();
+                            $('#user_bv_remarks_edit').modal('hide');
+                            Swal.fire("Good job!", "You clicked the button!", "success");
+                            // swal.fire({
+                            //     title: "updated",
+                            //     text: response.message,
+                            //     icon: 'success',
+                            //     type: "success",
+                            //     timer: 3000
+                            // });
+                            console.log(response);
+                            alert("Remarks Updated Successfully!");
+                            $('.form-group').removeClass('has-error')
+                                .removeClass('has-success');
+                            $('.text-danger').remove();
+                            // $('#fetch_user_data').DataTable().ajax.reload();
+                            // reset the form
+                            me[0].reset();
+
+                        } else if (response.error == true) {
+                            $('#user_bv_remarks_edit').modal('hide');
+                            Swal.fire("Good job!", "You clicked the button!", "error");
+                            // swal.fire({
+                            //     title: "Try Again ! ",
+                            //     text: response.message,
+                            //     icon: 'error',
+                            //     type: "error",
+                            //     timer: 3000
+                            // });
+                            $('#user_bv_remarks_edit').modal('hide');
+                            //console.log(response);
+                            $('.form-group').removeClass('has-error')
+                                .removeClass('has-success');
+                            $('.text-danger').remove();
+                            // $('#teacher_add_model').modal('hide');
+                            // $('#fetch_user_data').DataTable().ajax.reload();
+                            // reset the form
+                            me[0].reset();
+                        } else {
+                            $.each(response.messages, function(key, value) {
+                                var element = $('#u_' + key);
+
+                                element.closest('div.form-group')
+                                    .removeClass('has-error')
+                                    .addClass(value.length > 0 ? 'has-error' : 'has-success')
+                                    .find('.text-danger')
+                                    .remove();
+                                element.after(value);
+                            });
+                        }
+                    },
+                    error: function(xhr, ajaxOptions, thrownError) {
+                        //                            swal.fire("Error deleting!", "Please try again later !!!", "error");
+                        $('#user_bv_remarks_edit').modal('hide');
+                        swal.fire({
+                            title: "Error saving...",
+                            text: "Please try again later !!!",
+                            icon: 'error',
+                            type: "error",
+                            timer: 3000
+                        });
+                    }
+                });
+            });
+
+            // end update form data 
+            $(document).on('click', '.edit_bv', function() {
+                var user_id = $(this).attr("id");
+                $.ajax({
+                    url: "<?php echo base_url(); ?>View_mini_case_controller/fetch_bv_remarks",
+                    method: "POST",
+                    data: {
+                        user_id: user_id
+                    },
+                    dataType: "json",
+                    success: function(data) {
+                        $('#user_bv_remarks_edit').modal('show');
+                        $('#u_bv_remarks').val(data.rv_remarks);
+                        $('#u_bv_id').val(user_id);
+                        $('#update_bv_remarks').val("edit");
                     }
                 });
             });
@@ -374,23 +476,23 @@
         // });
 
         // end update form data 
-        $(document).on('click', '.edit_rv', function() {
-            var user_id = $(this).attr("id");
-            $.ajax({
-                url: "<?php echo base_url(); ?>View_mini_case_controller/fetch_rv_remarks",
-                method: "POST",
-                data: {
-                    user_id: user_id
-                },
-                dataType: "json",
-                success: function(data) {
-                    $('#user_rv_remarks_edit').modal('show');
-                    $('#u_rv_remarks').val(data.rv_remarks);
-                    $('#u_rv_id').val(user_id);
-                    $('#update_rv_remarks').val("edit");
-                }
-            });
-        });
+    //   $(document).on('click', '.edit_rv', function() {
+        //     var user_id = $(this).attr("id");
+        //     $.ajax({
+        //         url: "<?php echo base_url(); ?>View_mini_case_controller/fetch_rv_remarks",
+        //         method: "POST",
+        //         data: {
+        //             user_id: user_id
+        //         },
+        //         dataType: "json",
+        //         success: function(data) {
+        //             $('#user_rv_remarks_edit').modal('show');
+        //             $('#u_rv_remarks').val(data.rv_remarks);
+        //             $('#u_rv_id').val(user_id);
+        //             $('#update_rv_remarks').val("edit");
+        //         }
+        //     });
+        // });
 
         $(document).on('click', '.edit_bv', function() {
             var user_id = $(this).attr("id");
@@ -1044,28 +1146,28 @@
     </div>
 
 
-    <!--<div class="modal fade" id="user_bv_remarks_edit" tabindex="-1" role="dialog" aria-hidden="true">-->
-    <!--    <div class="modal-dialog" role="document">-->
-    <!--        <div class="modal-content">-->
-    <!--            <div class="modal-header">-->
-    <!--                <h5 class="modal-title">Update BV Remarks</h5>-->
-    <!--            </div>-->
-    <!--            <?php echo form_open("View_mini_case_controller/update_bv_remarks_validation", array("id" => "update_rv_remarks", "class" => "form-horizontal")) ?>-->
-    <!--            <div class="modal-body">-->
-    <!--                <div class="form-row">-->
-    <!--                    <input type="hidden" class="form-control" id="u_rv_id" name="rv_id">-->
+    <div class="modal fade" id="user_bv_remarks_edit" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Update BV Remarks</h5>
+                </div>
+                <?php echo form_open("View_mini_case_controller/update_bv_remarks_validation", array("id" => "update_bv_remarks", "class" => "form-horizontal")) ?>
+                <div class="modal-body">
+                    <div class="form-row">
+                        <input type="hidden" class="form-control" id="u_bv_id" name="bv_id">
 
-    <!--                    <div class="form-group col-md-12">-->
-    <!--                        <label for="rv_remarks" class="h5">BV Remarks</label>-->
-    <!--                        <textarea type="text" class="form-control" id="u_rv_remarks" placeholder="Enter Rv Remarks" name="rv_remarks"></textarea>-->
-    <!--                    </div>-->
-    <!--                </div>-->
-    <!--            </div>-->
-    <!--            <div class="modal-footer">-->
-    <!--                <input type="submit" name="update_rv_remarks" id="update_rvr_data" class="btn btn-primary" value="Edit">-->
-    <!--                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>-->
-    <!--            </div>-->
-    <!--        </div>-->
-    <!--        <?php echo form_close(); ?>-->
-    <!--    </div>-->
-    <!--</div>-->
+                        <div class="form-group col-md-12">
+                            <label for="bv_remarks" class="h5">BV Remarks</label>
+                            <textarea type="text" class="form-control" id="u_bv_remarks" placeholder="Enter bv Remarks" name="bv_remarks"></textarea>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <input type="submit" name="update_bv_remarks" id="update_bvr_data" class="btn btn-primary" value="Edit">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+            <?php echo form_close(); ?>
+        </div>
+    </div>
